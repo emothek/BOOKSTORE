fc5c774268426bc090f92d042aecbfdbad427feb
emothek <zaki.sabile@gmail.com>
2016-11-16 17:57:23 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,143 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+
+app.use(express.static(__dirname+'/client')) // static public directory 
+
+app.use(bodyParser.json());  // middleware to intialize bodyParser
+
+Genre = require('./models/genres');
+Book = require('./models/books');
+
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/bookstore');
+
+var db = mongoose.connection;
+
+
+app.get('/', function(req, res){
+	res.send('Please use /api/books or /api/genres');
+});    // ROUTE of HOME PAGE : handle request (get/put/post/delete)  http request
+
+
+
+// Genres
+
+app.get('/api/genres', function(req, res){
+	Genre.getGenres(function(err, genres){
+		if(err){ 
+			throw err; 
+		}
+		res.json(genres);
+	});
+});
+
+
+
+app.post('/api/genres', function(req, res){
+	var genre = req.body;  // bodyParser form !
+	Genre.addGenre(genre, function(err, genre){
+		if(err){ 
+			throw err; 
+		}
+		res.json(genre);
+	});
+});
+
+
+app.put('/api/genres/:_id', function(req, res){
+	var id = req.params._id
+	var genre = req.body;  // bodyParser form !
+	
+	Genre.updateGenre(id, genre, {}, function(err, genre){
+		if(err){ 
+			throw err; 
+		}
+		res.json(genre);
+	});
+});
+
+
+app.delete('/api/genres/:_id', function(req, res){
+	var id = req.params._id
+	
+	Genre.deleteGenre(id, function(err, genre){
+		if(err){ 
+			throw err; 
+		}
+		res.json(genre);
+	});
+});
+
+
+
+
+
+
+//    Books
+
+app.post('/api/books', function(req, res){
+	var book = req.body;  // bodyParser form !
+	Book.addBook(book, function(err, book){
+		if(err){ 
+			throw err; 
+		}
+		res.json(book);
+	});
+});
+
+
+app.get('/api/books', function(req, res){
+	Book.getBooks(function(err, books){
+		if(err){ 
+			throw err; 
+		}
+		res.json(books);
+	});
+});
+
+app.get('/api/books/:_id', function(req, res){
+	Book.getBookById(req.params._id, function(err, book){
+		if(err){ 
+			throw err; 
+		}
+		res.json(book);
+	});
+});
+
+
+
+app.put('/api/books/:_id', function(req, res){
+	var id = req.params._id
+	var book = req.body;  // bodyParser form !
+	
+	Book.updateBook(id, book, {}, function(err, book){
+		if(err){ 
+			throw err; 
+		}
+		res.json(book);
+	});
+});
+
+
+
+
+app.delete('/api/books/:_id', function(req, res){
+	var id = req.params._id
+	
+	Book.deleteBook(id, function(err, book){
+		if(err){ 
+			throw err; 
+		}
+		res.json(book);
+	});
+});
+
+
+
+
+app.listen(3000);
+
+console.log('App running on P 3000');
